9c4c9d5d8298af2b258c32ab810d5e3ea29453ad
Deep Patel <DP1852001@GMAIL.COM>
2026-07-17 15:40:43 +0200

    Update app.py

--- diabetes-prediction-ml/app.py
+++ diabetes-prediction-ml/app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import numpy as np
 import pickle
+from sklearn import svm                      
+from sklearn.preprocessing import StandardScaler
 
 # Load the saved model and scaler
-model = pickle.load(open('diabetes_model.sav', 'rb'))
-scaler = pickle.load(open('scaler.sav', 'rb'))
+model = pickle.load(open('Models/diabetes_model.sav', 'rb'))
+scaler = pickle.load(open('Models/scaler.sav', 'rb'))
 
 # Set up page configuration
 st.set_page_config(page_title="Diabetes Prediction Dashboard", layout="centered")
